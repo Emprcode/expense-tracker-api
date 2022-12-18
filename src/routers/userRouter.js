9d47642156bd303eb8e93cec2e2ca91190adcb9b
Narayan Aryal <aryal.coding@gmail.com>
2022-12-18 14:50:19 +1100

    api done

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,6 +8,8 @@ router.all("/", (req, res, next) => {
   next();
 });
 
+
+//creating users
 router.post("/", async (req, res, next) => {
   try {
     const result = await createUser(req.body);
@@ -29,5 +31,30 @@ router.post("/", async (req, res, next) => {
     next(error);
   }
 });
+
+
+//login users
+router.post("/login", async (req, res, next) => {
+  try {
+    const result = await createUser(req.body);
+    result?._id
+      ? res.json({
+          status: "OK",
+          message: "Insert user done Successfully",
+        })
+      : res.json({
+          status: "error",
+          message: "unable to add user, please try again",
+        });
+  } catch (error) {
+    if (error.message.includes("E11000 duplicate key error collection")) {
+      error.errorCode = 200;
+      error.message =
+        "This email is already in use, Please use different email";
+    }
+    next(error);
+  }
+});
+
 
 export default router;
